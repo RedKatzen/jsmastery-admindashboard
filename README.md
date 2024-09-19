04947e6623b51c87521c5331ea41ee3a8ce509fc
RedKatzen <gabrieldeavilaf@gmail.com.br>
2024-09-19 10:52:34 -0300

    Modifying README

---
+++ README.md
@@ -0,0 +1,7 @@
+# Building  
+![Shoppy](https://i.ibb.co/W6g39w3/image.png)
+
+## Introduction
+A React Admin Dashboard app with charts, tables, calendar and others - with a components library [Syncfusion](https://www.syncfusion.com/)
+
+### Following the [Java Script Mastery](https://www.youtube.com/@javascriptmastery) tutorial -> https://www.youtube.com/watch?v=jx5hdo50a2M&t=2830s
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,11 +5,16 @@ import { BsChatLeft } from 'react-icons/bs';
 import { RiNotification3Line } from 'react-icons/ri';
 import { TooltipComponent } from '@syncfusion/ej2-react-popups';
 import avatar from '../data/avatar.jpg';
-
+import { Cart, Chat, Notifications, UserProfile } from '.';
+import { useStateContext } from '../context/ContextProvider';
 
 const Navbar = () => {
+  const { activeMenu, setActiveMenu } = useStateContext();
+
   return (
-    <div>Navbar</div>
+    <div className='flex justify-between p-2 md:mx-6 relative'>
+      
+    </div>
   )
 }
 
