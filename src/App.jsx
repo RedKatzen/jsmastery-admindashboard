a9bb163ccd627b2bbdcf4a23f213e896ee23c261
RedKatzen <gabrieldeavilaf@gmail.com.br>
2024-10-21 16:41:51 -0300

    feat: Editor page

--- src/App.jsx
+++ src/App.jsx
@@ -69,7 +69,7 @@ const App = () => {
                 <Route path="/customers" element={<Customers />} />
 
                 <Route path="/kanban" element={<Kanban />} />
-                <Route path="/editor " element={<Editor />} />
+                <Route path="/editor" element={<Editor />} />
                 <Route path="/calendar" element={<Calendar />} />
                 <Route path="/color-picker " element={<ColorPicker />} />
 
